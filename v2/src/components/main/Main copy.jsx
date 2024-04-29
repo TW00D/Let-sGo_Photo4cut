0ed1a44663f7085cff2644a424012d02666ea007
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2024-04-29 21:53:24 +0900

    feat : ai 기능개발

--- v2/src/components/main/Main copy.jsx
+++ v2/src/components/main/Main copy.jsx
@@ -1,15 +1,30 @@
-import React, { useEffect, useRef, useState } from "react";
-import * as M from "./Main.style";
-import axios from "axios";
+// import React, { useEffect, useRef, useState } from "react";
+// import * as M from "./Main.style";
 
-import "@tensorflow/tfjs-core";
-import "@tensorflow/tfjs-converter";
-import "@tensorflow/tfjs-backend-webgl";
-import * as bodyPix from "@tensorflow-models/body-pix";
-import Webcam from "react-webcam";
+// import "@tensorflow/tfjs-core";
+// import "@tensorflow/tfjs-converter";
+// import "@tensorflow/tfjs-backend-webgl";
+// import * as bodyPix from "@tensorflow-models/body-pix";
+// import Webcam from "react-webcam";
 
-import logo from "../../assets/logo.svg";
-import takePicture from "../../assets/takePicture.svg";
+// import logo from "../../assets/logo.svg";
+// import takePicture from "../../assets/takePicture.svg";
+
+// import schoolBackImg from "../../assets/dgswback.jpg";
+// import schoolFrontImg from "../../assets/main.jpg";
+// import playGroundImg from "../../assets/school.jpg";
+// import setBackground from "../../assets/setBackground.svg";
+// import setFilter from "../../assets/setFilter.svg";
+
+// //import lupi from "../../assets/lupi.png"
+// import dinosaur from "../../assets/dinosaur.jpg";
+// import cb from "../../assets/cb.jpg";
+// import jjanggu from "../../assets/jjanggu1.jpg";
+// import spongibab from "../../assets/spongibab.jpg";
+// import lupi from "../../assets/lupi.png";
+// import { useNavigate } from "react-router-dom";
+// import { useRecoilState } from "recoil";
+// import { imageState } from "../../global/image";
 
 // const Main = () => {
 //   const canvasRef = useRef(null);
@@ -268,6 +283,9 @@ import takePicture from "../../assets/takePicture.svg";
 
 // export default Main;
 
+// import "./styles.css";
+import React, { useState, useEffect, useRef } from "react";
+
 export default function Main() {
   const videoRef = useRef(null);
 
@@ -322,20 +340,9 @@ export default function Main() {
     context.drawImage(video, 0, 0, "1024", "768");
     canvas.toBlob((blob) => {
       //캔버스의 이미지를 파일 객체로 만드는 과정
-      let file = new File([blob], "fileName.jpg", { type: "image/png" });
-      const formData = new FormData();
-      formData.append("image", file);
+      let file = new File([blob], "fileName.jpg", { type: "image/jpeg" });
       const uploadFile = [file]; //이미지 객체
-
-      axios
-        .post("http://10.72.160.85:5000/generate", formData)
-        .then((response) => {
-          console.log("File uploaded successfully:", response);
-        })
-        .catch((error) => {
-          console.error("Error uploading file:", error);
-        });
-    }, "image/png");
+    }, "image/jpeg");
 
     const image = canvas.toDataURL(); // 이미지 저장하는 코드
     const link = document.createElement("a");
@@ -352,87 +359,83 @@ export default function Main() {
   return (
     <M.Wrapper>
       <M.CamWrapper>
-        <video
-          id="videoCam"
-          ref={videoRef}
-          autoPlay
-          style={{
-            display: CameraState,
-            width: "1280px",
-            height: "720px",
-            webkitTransform: "rotateY(180deg)",
-          }}
+        <Webcam
+          ref={webcamRef}
+          audio={false}
+          width={1280}
+          height={720}
+          screenshotFormat="image/jpeg"
+          className="webcam"
+          //   videoConstraints={videoConstraints}
         />
-        <canvas
-          id="canvas"
-          width="1280px"
-          height="720px"
-          style={{ display: CanvasState }}
-        ></canvas>
+        <canvas ref={canvasRef} className="canvas" />
       </M.CamWrapper>
       <M.ButtonWrapper>
         <M.Header>
           <M.HeaderImg src={logo}></M.HeaderImg>
         </M.Header>
+        <div>
+          <div>
+            <M.TextImg src={setBackground}></M.TextImg>
+            <div>
+              <M.Button onClick={() => clickHandler(dinosaur)}>
+                쥬라기 스쿨
+              </M.Button>
+              <M.Button onClick={() => clickHandler(spongibab)}>
+                스폰지밥
+              </M.Button>
+              <M.Button onClick={() => clickHandler(playGroundImg)}>
+                학교 운동장
+              </M.Button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <M.Button onClick={() => clickHandler(schoolFrontImg)}>
+                학교 기숙사동
+              </M.Button>
+              <M.Button onClick={() => clickHandler(schoolBackImg)}>
+                학교 크로마키
+              </M.Button>
+              <M.Button onClick={() => clickHandler(cb)}>벚꽃</M.Button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <M.TextImg2 src={setFilter}></M.TextImg2>
 
-        {/* <M.TakeButton
+          <div>
+            <M.Button onClick={() => clickHandler(dinosaur)}>
+              쥬라기 스쿨
+            </M.Button>
+            <M.Button onClick={() => clickHandler(jjanggu)}>짱구</M.Button>
+            <M.Button onClick={() => clickHandler(playGroundImg)}>
+              학교 운동장
+            </M.Button>
+          </div>
+          <div>
+            <M.Button onClick={() => clickHandler(lupi)}>잔망루피</M.Button>
+            <M.Button onClick={() => clickHandler(spongibab)}>
+              스폰지밥
+            </M.Button>
+            <M.Button onClick={() => clickHandler(playGroundImg)}>
+              학교 운동장
+            </M.Button>
+          </div>
+        </div>
+
+        {/* <M.Button onClick={() => resetRAF()}>배경 없애기</M.Button> */}
+        <M.TakeButton
           src={takePicture}
           onClick={() => snapshot()}
         ></M.TakeButton>
         <img src={takePicture} onClick={() => snapshot()}></img>
-        <M.TakeButton src={takePicture}></M.TakeButton> */}
-
-        {CanvasState === "none" ? (
-          <div
-            onClick={sreenShot}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "70px",
-              height: "70px",
-              margin: "10px",
-              borderRadius: "100px",
-              position: "absolute",
-              zIndex: "101",
-              bottom: "5%",
-              left: "46%",
-              cursor: "pointer",
-              backgroundColor: "white",
-            }}
-          >
-            <div
-              style={{
-                textAlign: "center",
-                width: "60px",
-                height: "60px",
-                border: "2px solid",
-                borderRadius: "100px",
-              }}
-            ></div>
-          </div>
-        ) : (
-          <div
-            onClick={GoToCamera}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "70px",
-              height: "70px",
-              margin: "10px",
-              borderRadius: "10px",
-              position: "absolute",
-              zIndex: "101",
-              bottom: "5%",
-              left: "46%",
-              cursor: "pointer",
-              backgroundColor: "white",
-            }}
-          >
-            <p>다시 촬영</p>
-          </div>
-        )}
+        <M.TakeButton src={takePicture}></M.TakeButton>
+        {/* {
+            image.map((e,idx) => (
+                <img src={e} key={idx} />
+            ))
+        } */}
       </M.ButtonWrapper>
     </M.Wrapper>
   );
--- v2/src/components/main/Main.style.js
+++ v2/src/components/main/Main.style.js
@@ -1,66 +1,64 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    width:100%;
-    height:100vh;
-    display:flex;
-    box-sizing:border-box;
-    position:relative;
-    background-color:white;
-
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  box-sizing: border-box;
+  position: relative;
+  background-color: white;
+`;
 export const CamWrapper = styled.div`
-    height:100%;
-    width:70%;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    .webcam{
-        border-radius:15px;
-        width:960px;
-        height:720px;
-    }
-    .canvas{
-        position:absolute;
-        width:960px;
-        height:720px;
-        border-radius:15px;
-    }
-`
+  height: 100%;
+  width: 70%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .webcam {
+    border-radius: 15px;
+    width: 960px;
+    height: 720px;
+  }
+  .canvas {
+    position: absolute;
+    width: 960px;
+    height: 720px;
+    border-radius: 15px;
+  }
+`;
 export const ButtonWrapper = styled.div`
-    height:100%;
-    width:30%;
-    display:flex;
-    flex-direction:column;
-    justify-content:space-around;
-    align-items:center;
-    background-color:white;
-`
+  height: 100%;
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+`;
 export const Header = styled.header`
-    display:Flex;
-    align-items:center;
-`
+  display: Flex;
+  align-items: center;
+`;
 export const HeaderImg = styled.img`
-    width:200px;
-`
+  width: 5vw;
+`;
 export const TextImg = styled.img`
-    width:100px;
-`
+  width: 100px;
+`;
 export const TextImg2 = styled.img`
-    width:70px;
-`
+  width: 70px;
+`;
 export const Title = styled.h1`
-    color:white;
-`
+  color: white;
+`;
 
 export const Button = styled.button`
-      cursor: pointer;
+  cursor: pointer;
   width: 100px;
   height: 100px;
   outline: none;
   border: 0px;
   background-color: #f2f2f2;
-  margin:10px;
-`
-export const TakeButton = styled.image`
-`
+  margin: 10px;
+`;
+export const TakeButton = styled.image``;
